f280c57b5e1d2b99b92701e398ae88fdc3ac46f6
janasluo <janas_luo@126.com>
2020-07-16 15:31:01 +0800

    代理配置

---
+++ config/proxy.conf.js
@@ -0,0 +1,12 @@
+const target = require('./target')
+// const target = "http://192.168.18.93:8080"
+module.exports = {
+  "/api": {
+    "target": target,
+    "changeOrigin": true,
+    "ws": false,
+    "pathRewrite": {
+      "^/api": "/api"
+    }
+  }
+}
